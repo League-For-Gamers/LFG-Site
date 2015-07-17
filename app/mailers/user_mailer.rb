8aa2600b92b1cde55bccd3beda0b11554c689d7d
Emily Ozols <wingar@team-metro.net>
2015-07-17 18:10:16 +1000

    Password recovery mail had an invalid date. Fixed LFG-25.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,6 +2,7 @@ class UserMailer < ApplicationMailer
   def recovery_email(user)
     @user = user
     @recovery_link = user.generate_password_reset_link
+    @time = Time.now
     mail to: @user.decrypted_email, subject: "League for Gamers password recovery"
   end
 end
