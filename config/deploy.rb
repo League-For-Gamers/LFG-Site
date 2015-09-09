da0e665d86a713db7c0f56f0722f73341f417d54
Emily Ozols <wingar@team-metro.net>
2015-09-09 14:49:43 +1000

    Forgot to actually link newrelic config in capistrano.

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,7 +26,7 @@ set :format, :pretty
 set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/application.yml')
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/application.yml', 'config/newrelic.yml')
 
 # Default value for linked_dirs is []
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/sessions', 'tmp/sockets','tmp/pids', 'vendor/bundle')
