92dbd5b26d3016a96d94b80b5f32c98f1a81ae20
Emily Ozols <wingar@team-metro.net>
2015-09-26 22:29:20 +1000

    Added in redis session expire time.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
 if Rails.env.production? or Rails.env.staging?
-  Rails.application.config.session_store :redis_store, servers: ENV['REDIS_SESSION_ADDRESS']
+  Rails.application.config.session_store :redis_store, servers: ENV['REDIS_SESSION_ADDRESS'], expire_in: 86400 # 1 day
 else
   Rails.application.config.session_store :cookie_store, key: '_LeagueForGamers_session'
 end
