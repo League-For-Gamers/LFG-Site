0867beb3a33c0fbc85d095acaa745884c6715b9f
Darren Cauthon <darren@cauthon.com>
2016-07-04 19:31:47 +0000

    These are the first variables we care about.

--- app/controllers/omni_controller.rb
+++ app/controllers/omni_controller.rb
@@ -1,10 +1,18 @@
 class OmniController < ApplicationController
   def create
-    raise [auth_hash[:extra][:access_token].to_json].inspect
+    raise [access_token.token, access_token.secret, raw_info.screen_name].inspect
     redirect_to '/'
   end
 
   protected
+
+  def access_token
+    auth_hash[:extra][:access_token]
+  end
+
+  def raw_info
+    auth_hash[:extra][:raw_info]
+  end
 
   def auth_hash
     request.env['omniauth.auth']
